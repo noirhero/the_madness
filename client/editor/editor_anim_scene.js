49ecf14827c060ae920a773b4b9d5ca46362156f
nhlee <noirhero83@gmail.com>
2018-11-18 23:00:32 +0900

    + Edit animation state and duration.

--- client/editor/editor_anim_scene.js
+++ client/editor/editor_anim_scene.js
@@ -35,9 +35,78 @@ class EditorAnimScene extends Scene {
 
             entity.addComponent(new ComponentAnim(anim_url));
 
-            contolKit.addPanel({
+            const panel = contolKit.addPanel({
               fixed: false,
               label: file.name,
+              position: [0, 0],
+            });
+
+            const group_anims = panel.addGroup({
+              label: "Animations",
+            });
+
+            const temp_anim_data = {}
+            for(let i in anim_data) {
+              temp_anim_data[i] = {
+                name: i,
+                frames: {},
+              };
+
+              const anim_info = anim_data[i];
+              const num_frames = anim_info.frames.length;
+              for(let i_frame = 0; i_frame < num_frames; ++i_frame) {
+                const frame_info = anim_info.frames[i_frame];
+                temp_anim_data[i].frames[i_frame] = frame_info.end - frame_info.start;
+              }
+
+              const sub_group = group_anims.addSubGroup({
+                label: i,
+              }).addButton("PLAY", function() {
+                const comp_anim = entity.getComponent("Anim");
+                const comp_anim_data = comp_anim.anim.GetData();
+                if(!comp_anim_data[i]) {
+                  comp_anim.state = temp_anim_data[i].name;
+                }
+                else {
+                  comp_anim.state = i;
+                }
+                comp_anim.duration = 0;
+              }).addStringInput(temp_anim_data[i], "name");
+
+              for(let i_frame = 0; i_frame < num_frames; ++i_frame) {
+                sub_group.addNumberInput(temp_anim_data[i].frames, i_frame);
+              }
+            }
+
+            panel.addGroup({
+              label: "Management"
+            }).addButton("STOP", function() {
+
+            }).addButton("APPLY", function() {
+              const comp_anim_data = entity.getComponent("Anim").anim.GetData();
+              for(const i in comp_anim_data) {
+                if(!temp_anim_data[i]) {
+                  continue;
+                }
+
+                let total_duration = 0;
+                const dest_frames = temp_anim_data[i].frames;
+                const src_frames = comp_anim_data[i].frames;
+                const num_src_frames = src_frames.length;
+                for(let i_frame = 0; i_frame < num_src_frames; ++i_frame) {
+                  src_frames[i_frame].start = total_duration;
+                  total_duration += dest_frames[i_frame];
+                  src_frames[i_frame].end = total_duration;
+                }
+                comp_anim_data[i].total_duration = total_duration;
+
+                if(i == temp_anim_data[i].name) {
+                  continue;
+                }
+
+                comp_anim_data[temp_anim_data[i].name] = comp_anim_data[i];
+                delete comp_anim_data[i];
+              }
             }).addButton("SAVE", function() {
               const comp_anim = entity.getComponent("Anim");
               const blob = new Blob([JSON.stringify(comp_anim.anim.GetData(), null, " ")], {type: "application/json"});
@@ -48,6 +117,7 @@ class EditorAnimScene extends Scene {
               element_save.click();
             });
           };
+
           reader.readAsText(file);
         }
         else {
