738058419d5cb7dd17f8924ebd741769e342df70
nhlee <noirhero83@gmail.com>
2018-12-20 01:05:00 +0900

    + Support NPOT texture.

--- client/core/texture.js
+++ client/core/texture.js
@@ -16,6 +16,8 @@ function Texture(url) {
     GL.texImage2D(GL.TEXTURE_2D, 0, GL.RGBA, GL.RGBA, GL.UNSIGNED_BYTE, image);
     GL.texParameteri(GL.TEXTURE_2D, GL.TEXTURE_MAG_FILTER, GL.NEAREST);
     GL.texParameteri(GL.TEXTURE_2D, GL.TEXTURE_MIN_FILTER, GL.NEAREST);
+    GL.texParameteri(GL.TEXTURE_2D, GL.TEXTURE_WRAP_S, GL.CLAMP_TO_EDGE);
+    GL.texParameteri(GL.TEXTURE_2D, GL.TEXTURE_WRAP_T, GL.CLAMP_TO_EDGE);
     GL.bindTexture(GL.TEXTURE_2D, null);
 
     image = null;
