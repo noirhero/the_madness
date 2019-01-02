798b1d6a2df058e85e287103f58991c7ea00310a
nhlee <noirhero83@gmail.com>
2019-01-03 00:41:22 +0900

    + Code cleanup.

--- client/system/system_movement_net_player.js
+++ client/system/system_movement_net_player.js
@@ -14,7 +14,8 @@ const SystemMovementNetPlayer = CES.System.extend({
     let sync_data = sync_buffer.shift();
     while(sync_data) {
       const sync_id = sync_data.id;
-      const sync_pos = [sync_data.x, sync_data.y, 0];
+      const x = sync_data.x;
+      const y = sync_data.y;
 
       let net_player_entity = null;
       this.world.getEntities("NetPlayer", "Pos").some(entity => {
@@ -32,8 +33,8 @@ const SystemMovementNetPlayer = CES.System.extend({
             const pos_comp = load_entity.getComponent("Pos");
             if(pos_comp) {
               const pos = pos_comp.pos;
-              pos[0] = sync_pos[0];
-              pos[1] = sync_pos[1];
+              pos[0] = x;
+              pos[1] = y;
               load_entity.addComponent(new ComponentPlayerNet(sync_id, glMatrix.vec3.fromValues(pos[0], pos[1], pos[2])));
             }
             else {
@@ -45,8 +46,9 @@ const SystemMovementNetPlayer = CES.System.extend({
         });
       }
       else {
-        const net_player_comp = net_player_entity.getComponent("NetPlayer");
-        glMatrix.vec3.copy(net_player_comp.net_pos, sync_pos);
+        const net_pos = net_player_entity.getComponent("NetPlayer").net_pos;
+        net_pos[0] = x;
+        net_pos[1] = y;
       }
 
       sync_data = sync_buffer.shift();
