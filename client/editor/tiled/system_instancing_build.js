f500cf91be29def2163accba4ce4dfc1cbb95fc7
nhlee <noirhero83@gmail.com>
2019-03-11 01:56:53 +0900

    + Working on instancing.

--- client/editor/tiled/system_instancing_build.js
+++ client/editor/tiled/system_instancing_build.js
@@ -5,6 +5,9 @@ const SystemInstancingBuild = CES.System.extend({
     const world = this.world;
     const instancing_comp = new ComponentInstancing();
 
+    const world_transform = glMatrix.mat4.create();
+    const world_pos = glMatrix.vec3.create();
+
     world.getEntities("InstancingBuild").forEach(entity => {
       const build_comp = entity.getComponent("InstancingBuild");
       const check_box = (new SAT.Box(new SAT.V(build_comp.x, build_comp.y), build_comp.w, build_comp.h)).toPolygon();
@@ -19,16 +22,61 @@ const SystemInstancingBuild = CES.System.extend({
           return;
         }
 
+        const tex_coord = mesh_entity.getComponent("Texcoord").values;
+        const texture = mesh_entity.getComponent("Texture").texture;
+        if(0 === instancing_comp.subsets.length) {
+          instancing_comp.subsets[instancing_comp.subsets.length] = {
+            vertices: [],
+            textures: [texture],
+            vb: null,
+          };
+        }
+
+        let subset = instancing_comp.subsets[instancing_comp.subsets.length - 1];
+        if(subset.vertices.length >= (NUM_BATCH * 6/*two polygon*/)) {
+          subset = instancing_comp.subsets[instancing_comp.subsets.length] = {
+            vertices: [],
+            textures: [texture],
+            vb: null,
+          };
+        }
+
+        let texture_idx = 0;
+        if(false === subset.textures.some(i => {
+          if(i === texture) {
+            texture_idx = i;
+            return true;
+          }
+
+          return false;
+        })) {
+          if(LIMIT_TEXTURE === subset.textures.length) {
+            subset = instancing_comp.subsets[instancing_comp.subsets.length] = {
+              vertices: [],
+              textures: [texture],
+              vb: null,
+            };
+          }
+          else {
+            texture_idx = subset.textures.length;
+            subset.textures[subset.textures.length] = texture;
+          }
+        }
+
         const scale = mesh_entity.getComponent("Scale").scale;
         const rot = mesh_entity.getComponent("Rot") ? mesh_entity.getComponent("Rot").rot : IDENTITY_QUAT;
-        const texture = mesh_entity.getComponent("Texture").texture;
+        glMatrix.mat4.fromRotationTranslationScale(world_transform, rot, pos, scale);
 
-        let subset = null;
-        if(0 === instancing_comp.subsets.length) {
-          subset = instancing_comp.subsets[instancing_comp.subsets.length] = {};
-        }
-        else {
-          subset = instancing_comp.subsets[instancing_comp.subsets.length];
+        const vertices = subset.vertices;
+        for(let i = 0; i < 4; ++i) {
+          glMatrix.vec3.transformMat4(world_pos, QUAD_LOCAL_POS[i], world_transform);
+
+          vertices[vertices.length] = world_pos[0];
+          vertices[vertices.length] = world_pos[1];
+          vertices[vertices.length] = pos[2];
+          vertices[vertices.length] = tex_coord[i][0];
+          vertices[vertices.length] = tex_coord[i][1];
+          vertices[vertices.length] = texture_idx;
         }
 
         world.removeEntity(mesh_entity);
